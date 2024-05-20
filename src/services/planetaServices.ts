af31f8fc4eaf5512b899313326c549cacb59373b
Bárbara Anger <barbaraanger@hotmail.com>
2024-05-20 11:24:21 -0300

    adiciona catch a requisição

--- src/services/planetaServices.ts
+++ src/services/planetaServices.ts
@@ -1,21 +1,26 @@
-import { Request, Response } from "express";
 import { client } from "../client";
 import { Planeta } from "../models/planetas";
 
 class PlanetaServices {
-  public static async getAllPlanets(req: Request, res: Response): Promise<any> {
+  public static async getAllPlanets(): Promise<any> {
     const listaPlanetas: Planeta[] = [];
-    client.get("/planets/").then(async (response) => {
-      while (response.data.next) {
-        listaPlanetas.push(...response.data.results);
-        response = await client.get(response.data.next);
-      }
-    });
-    if (listaPlanetas.length > 0) {
-      return listaPlanetas;
-    } else {
-      return "Error fetching data";
-    }
+    client
+      .get("/planets/")
+      .then(async (response) => {
+        while (response.data.next) {
+          listaPlanetas.push(...response.data.results);
+          response = await client.get(response.data.next);
+        }
+
+        if (listaPlanetas.length > 0) {
+          return listaPlanetas;
+        } else {
+          return "Error fetching data";
+        }
+      })
+      .catch((error) => {
+        return `Error fetching data: ${error}`;
+      });
   }
 }
 
