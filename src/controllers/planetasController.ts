0e599288489cb80f6e47b8c95c32f0fbe45a2e56
wander06 <wandersonpsilva7@gmail.com>
2024-08-16 11:21:35 -0300

    Ajustando codigo de requisicao da api

--- src/controllers/planetasController.ts
+++ src/controllers/planetasController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import PlanetaServices from "../services/planetaServices";
 
 class PlanetasController {
-  static getAllPlanetas = (req: Request, res: Response) => {
+  static getAllPlanetas = async (req: Request, res: Response) => {
     // controller nao pode ter lógica de negócio, apenas chamar o service
     // ela serve para tratar os erros e deixá-los mais amigáveis para o usuário
     // deve-se usar condicionais, try catch, etc. para tratar os erros
     // e devolver uma resposta amigável para o usuário
     // pesquise sobre status code e como usá-los para tratar erros de forma eficiente e amigável
     // mas também atente-se para não expor informações sensíveis ao usuário
-    res.status(200).json(PlanetaServices.getAllPlanets());
+    res.status(200).json(await PlanetaServices.getAllPlanets());
   };
 }
 
--- src/services/planetaServices.ts
+++ src/services/planetaServices.ts
@@ -1,26 +1,30 @@
+
 import { client } from "../client";
 import { Planeta } from "../models/planetas";
 
 class PlanetaServices {
   public static async getAllPlanets(): Promise<any> {
-    const listaPlanetas: Planeta[] = [];
-    client
-      .get("/planets/")
-      .then(async (response) => {
-        while (response.data.next) {
-          listaPlanetas.push(...response.data.results);
-          response = await client.get(response.data.next);
-        }
+    let listaPlanetas: Planeta[] = [];
 
-        if (listaPlanetas.length > 0) {
-          return listaPlanetas;
-        } else {
-          return "Error fetching data";
-        }
-      })
-      .catch((error) => {
-        return `Error fetching data: ${error}`;
-      });
+    try {
+      let response = await client.get("/planets/")
+      listaPlanetas.push(response.data.results);
+
+      while (response.data.next) {
+        response = await client.get(response.data.next);
+        listaPlanetas.push(...response.data.results);
+      }
+
+      if (listaPlanetas.length > 0) {
+        return listaPlanetas;
+      } else {
+        return "Error fetching data";
+      }
+
+    } catch (error) {
+      return `Error fetching data: ${error}`;
+    }
+
   }
 }
 
